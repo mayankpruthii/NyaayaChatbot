d0c9425a1e6cc9413836eedc2f865e11c0d3b08d
Mayank <mayankpruthi123@gmail.com>
2020-06-18 15:34:42 +0530

    chatbot answering complete

--- chatbot.py
+++ chatbot.py
@@ -6,7 +6,7 @@ from Data_preprocessing import clean_the_question_content
 import pickle
 
 def read_data():
-    with open('./my_data.json') as json_file:
+    with open('./my_data1.json') as json_file:
         data = json.load(json_file)
 
     df = pd.DataFrame(data)
@@ -29,17 +29,19 @@ def printAnswer(question_embedding, sentence_embedding, df, sentences, question)
             max_sim = sim
             index_sim = index
 
-    if max_sim < 0.7:
+    if max_sim < 0.81:
         print("We'll get back to you soon!")
     else:
         print("\n")
         print("Question:", question)
         print("\n")
+        print("Similarty:", max_sim)
+        print("\n")
         print("Answer:", df.iloc[index_sim,0])
         print(df.iloc[index_sim,1])
 
 def questions_embedding_list(df):
-    bc = BertClient()
+    bc = BertClient(check_length=False)
     df_questions = get_sentences(df)
     df_question_bert_embeddings = []
     for sentence in df_questions:
@@ -50,17 +52,20 @@ def questions_embedding_list(df):
 
 
 def get_answer(question):
-    bc = BertClient()
+    bc = BertClient(check_length=False)
     question = clean_the_question_content(question)
     question_embedding = bc.encode([question])
     df = read_data()
     df_questions = get_sentences(df)
     
-    df_question_bert_embeddings = []
-    for sentence in df_questions:
-        df_question_bert_embeddings.append(bc.encode([sentence]))
-    
+    file = open("model.pkl", 'rb')
+    binary_data = file.read()
+    # sio = StringIO(binary_data)
+    df_question_bert_embeddings = pickle.loads(binary_data)
+
     printAnswer(question_embedding, df_question_bert_embeddings, df, df_questions, question)
 
 if __name__ == '__main__':
-    get_answer("cyber")
+    # df = read_data()
+    # questions_embedding_list(df)
+    get_answer("laws on covid 19")
